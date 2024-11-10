cf6ee3322d03466ffa4647ba3e21a5755d6a4fa2
Azaanahmadsiddiqui <160166682+Azaanahmadsiddiqui@users.noreply.github.com>
2024-11-10 20:58:43 +0500

    first commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div id='footer'
+     className='text-black mt-16 py-6 text-center bg-accent'>
+        @ Shehla | All rights reserved 2024.
+
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Skills.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+
+const Skills = () => {
+  return (
+    
+ 
+  
+    <div id='Skills' className='container pt-32'>
+      <div className='grid md:grid-cols-2 gap-20 items-center'>
+        <div data-aos="zoom-in-left">
+        <h1 className=' font-bold text-4xl md:text-10xl'>Technologies I work with</h1>
+<p className='text-red-500 font-bold pt-2'>
+  I have a solid foundation in web development, specializing in HTML, CSS, and Typescript
+</p>
+</div>
+<div className='grid grid-cols-2 text-accent text-4xl sm:text-4xl '>
+  <div className='space-y-2'>
+    <h2 data-aos="zoom-in-left">Html</h2>
+    <h2 data-aos="zoom-in-left">Css</h2>
+    <h2 data-aos="zoom-in-left">Typescript</h2>
+    
+  </div>
+  <div className='space-y-2'>
+    <h2 data-aos="zoom-in-left">Typing</h2>
+    <h2 data-aos="zoom-in-left">Next.js</h2>
+    <h2 data-aos="zoom-in-left">React.js</h2>
+
+  
+  
+
+   </div>
+</div>
+      </div>
+    </div>
+
+  )
+}
+
+export default Skills
